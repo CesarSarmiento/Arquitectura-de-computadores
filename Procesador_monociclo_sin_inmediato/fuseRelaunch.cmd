7103b691dc4b478730b5e916c2566ceda673cce4
Cesar A. Sarmiento <cesarmiento17@gmail.com>
2016-10-11 09:33:20 -0500

    Procesador1
    Procesador monociclo sin inmediato

---
+++ Procesador_monociclo_sin_inmediato/fuseRelaunch.cmd
@@ -0,0 +1,1 @@
+-intstyle "ise" -incremental -o "C:/Users/Cesar/Desktop/universidad/arquitectura_de_computadores/Xilinx/Procesador_monociclo_sin_inmediato/Test_Adder_isim_beh.exe" -prj "C:/Users/Cesar/Desktop/universidad/arquitectura_de_computadores/Xilinx/Procesador_monociclo_sin_inmediato/Test_Adder_beh.prj" "work.Test_Adder" 
