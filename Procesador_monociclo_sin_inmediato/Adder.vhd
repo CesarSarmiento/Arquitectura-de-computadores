c03ce57d75500ddaf24f8e589ca1a3ac7b1dc663
Cesar A. Sarmiento <cesarmiento17@gmail.com>
2016-10-11 09:19:18 -0500

    Procesar1
    Procesador1

---
+++ Procesador_monociclo_sin_inmediato/Adder.vhd
@@ -0,0 +1,49 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date:    19:27:13 10/10/2016 
+-- Design Name: 
+-- Module Name:    Adder - Behavioral 
+-- Project Name: 
+-- Target Devices: 
+-- Tool versions: 
+-- Description: 
+--
+-- Dependencies: 
+--
+-- Revision: 
+-- Revision 0.01 - File Created
+-- Additional Comments: 
+--
+----------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.STD_LOGIC_UNSIGNED.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx primitives in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity Adder is
+    Port ( Operador1 : in  STD_LOGIC_VECTOR (31 downto 0);
+           Operador2 : in  STD_LOGIC_VECTOR (31 downto 0);
+           ResultadoAdder : out  STD_LOGIC_VECTOR (31 downto 0));
+end Adder;
+
+architecture Behavioral of Adder is
+
+begin
+		process(Operador1,Operador2)
+		begin 
+		ResultadoAdder <= Operador1+Operador2;
+		
+		end process;
+
+end Behavioral;
+
