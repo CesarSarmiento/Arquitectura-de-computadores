64a060306d8ae48741d931b7085c29bcebd13993
Cesar A. Sarmiento <cesarmiento17@gmail.com>
2016-10-17 10:19:44 -0500

    Procesador1

---
+++ Nueva carpeta/Procesador1/Unidad_Control.vhd
@@ -0,0 +1,44 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date:    10:19:26 10/17/2016 
+-- Design Name: 
+-- Module Name:    Unidad_Control - Behavioral 
+-- Project Name: 
+-- Target Devices: 
+-- Tool versions: 
+-- Description: 
+--
+-- Dependencies: 
+--
+-- Revision: 
+-- Revision 0.01 - File Created
+-- Additional Comments: 
+--
+----------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx primitives in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity Unidad_Control is
+    Port ( Op : in  STD_LOGIC_VECTOR (1 downto 0);
+           Op3 : in  STD_LOGIC_VECTOR (5 downto 0);
+           Aluop : out  STD_LOGIC_VECTOR (5 downto 0));
+end Unidad_Control;
+
+architecture Behavioral of Unidad_Control is
+
+begin
+
+
+end Behavioral;
+
