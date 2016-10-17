f0287e69f62201979d311fc433f017faf935b383
Cesar A. Sarmiento <cesarmiento17@gmail.com>
2016-10-17 10:32:53 -0500

    Procesador1

--- Nueva carpeta/Procesador1/Unidad_Control.vhd
+++ Nueva carpeta/Procesador1/Unidad_Control.vhd
@@ -38,6 +38,43 @@ end Unidad_Control;
 architecture Behavioral of Unidad_Control is
 
 begin
+process(Op,Op3)
+begin
+if (Op= "10")then
+			case Op3 is 
+				
+							when "000000" => -- ADD
+								Aluop  <= "000001";
+
+							when "000100" => -- SUB
+								 Aluop <= "000010";
+								 
+							when "000001" => -- AND
+								Aluop <= "000011";
+								 
+							when "000101" => -- ANDN
+								Aluop <= "000100";
+								 
+							when "000010" => -- OR
+								Aluop <= "000101";
+								
+							when "000110" => -- ORN
+								Aluop <= "000110";
+								 
+							when "000011" => -- XOR
+								Aluop  <= "000111"; 
+								
+							when "000111" => -- XORN
+								Aluop <= "001000";
+								
+							when others => -- El resto de funciones
+								Aluop <= "111111";
+							
+						end case;
+						 
+		end if; 
+
+end process; 
 
 
 end Behavioral;
